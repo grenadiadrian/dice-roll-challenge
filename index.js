b3828898b765a81d660f2f2856de5539eb61b443
Adrian Mason <grenadiadrian@users.noreply.github.com>
2020-02-19 02:24:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ randomNumber1 = Math.floor(randomNumber1) + 1
 
 function dice1() {
   if (randomNumber1 === 1) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice1.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice1.png")
   } else if (randomNumber1 === 2) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice2.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice2.png")
   } else if (randomNumber1 === 3) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice3.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice3.png")
   } else if (randomNumber1 === 4) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice4.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice4.png")
   } else if (randomNumber1 === 5) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice5.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice5.png")
   } else {
-    document.getElementsByTagName("img")[0].setAttribute("src", "/images/dice6.png")
+    document.getElementsByTagName("img")[0].setAttribute("src", "images/dice6.png")
   }
 }
 
@@ -24,17 +24,17 @@ randomNumber2 = Math.floor(randomNumber2) + 1
 
 function dice2() {
   if (randomNumber2 === 1) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice1.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice1.png")
   } else if (randomNumber2 === 2) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice2.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice2.png")
   } else if (randomNumber2 === 3) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice3.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice3.png")
   } else if (randomNumber2 === 4) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice4.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice4.png")
   } else if (randomNumber2 === 5) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice5.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice5.png")
   } else {
-    document.getElementsByTagName("img")[1].setAttribute("src", "/images/dice6.png")
+    document.getElementsByTagName("img")[1].setAttribute("src", "images/dice6.png")
   }
 }
 
